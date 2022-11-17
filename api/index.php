16f17878e8d5d2f3ebcc744f149ffa3bb2d5c605
Ash <ashman.malik@outlook.com>
2022-11-17 13:18:28 +1100

    Added Sorting Filters

--- api/index.php
+++ api/index.php
@@ -94,14 +94,14 @@ $c_data = $myObject["data"];
   data-show-multi-sort="true"
   data-id-table="advancedTable"
   data-mobile-responsive="true"
-  data-sort-priority='[{"sortName": "id","sortOrder":"desc"}]'
+  data-sort-priority='[{"sortName": "name","sortOrder":"aesc"}]'
   data-check-on-init="true"> 
  
     <thead>
       <tr>
       	<th> Logos </th>
         <th data-field="id" data-sortable="true"> Connector id </th>
-        <th> Connector Name </th> 
+        <th data-field="name" data-sortable="true"> Connector Name </th> 
         <th> Connector Method </th>                
       </tr>
     </thead>
