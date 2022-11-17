1703c6682e0caf3c3a2f93526d3071ce49468ba3
Ash <ashman.malik@outlook.com>
2022-11-17 11:58:23 +1100

    Connectors EP

--- api/index.php
+++ api/index.php
@@ -1,7 +1,60 @@
 <?php
 
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+session_start(); 
+ob_start(); 
+
+$url = "https://au-api.basiq.io/token";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "basiq-version: 3.0",
+   "Content-Type: application/x-www-form-urlencoded",
+   "Authorization: Basic NzM4NDE4YjktNDdlYy00OGI2LTg5ODEtNjg0OGI3NzU2ZDczOjU1YWU5YjhiLTEzNDEtNDQxYi04M2IxLTg5ZGQ1MDYyYWNhNw==",
+   "Content-Length: 0",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+$server_token = curl_exec($curl);
+curl_close($curl);
+
+$server_obj = json_decode( $server_token );
+
+// //Print the array in a simple JSON format
+// echo '<pre>';
+// echo json_encode($server_token, JSON_PRETTY_PRINT);
+// echo '</pre>';
+
+
+// Calling connectors EP 
+
+$urls = "https://au-api.basiq.io/connectors";
+
+$curls = curl_init($urls);
+curl_setopt($curls, CURLOPT_URL, $urls);
+curl_setopt($curls, CURLOPT_POST, true);
+curl_setopt($curls, CURLOPT_RETURNTRANSFER, true);
+
+$headerss = array(
+   "Authorization: Bearer $server_obj->access_token",
+   "Content-Type: application/json",
+);
+curl_setopt($curls, CURLOPT_HTTPHEADER, $headerss);
+
+curl_setopt($curls, CURLOPT_POSTFIELDS, $datas);
+
+
+$resps = curl_exec($curls);
+curl_close($curls);
+
+$user_object = json_decode( $resps );
+
+var_dump($user_object);
+exit();
+exit; 
 
 
 ?>
